efc8336ffe8404ae8c1b60c6739561343afea716
jaineshtrivedi <43180810+jaineshtrivedi@users.noreply.github.com>
2019-07-03 11:23:39 +0200

    Update security_group.tf

--- security_group.tf
+++ security_group.tf
@@ -3,7 +3,7 @@
 resource "azurerm_network_security_group" "jenkins_security" {
   name                  = "${var.security_group_name}"
   location              = "${var.location}"
-  resource_group_name   = "${azurerm_resource_group.terraform_rg.name}"
+  resource_group_name   = "${var.resource_group_name}"
 
   security_rule {
         name                    = "AllowSSH"
